f99e55099b31d6c8c60526cccd3c5eb8afd6802c
Vaja Dipak <91632488+Vaja-Dipak@users.noreply.github.com>
2023-10-09 10:21:37 +0530

    Add files via upload

--- recursion_function_reverse_string.c
+++ recursion_function_reverse_string.c
@@ -1,9 +1,19 @@
 // WAP to find reverse of string using recursion  
 
 #include<stdio.h>
-int recursen(char nm[],int l)
+int reverse(char *nm)
 {
+	if(*nm)
+	{
+		reverse(nm+1);
+		printf("%c",*nm);
+	}	
 }
 main()
 {
+	char nm[20];
+	printf("Enter any string : ");
+	gets(nm);
+	reverse(nm);
+	return 0;
 }
